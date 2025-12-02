aa1070d99662f112ff6ec5a68cdab54af215d87e
Garun Vagidov <garun@garunski.com>
2025-12-02 16:32:46 -0500

    Update Vite configuration to set base path for GitHub Pages deployment, enhancing project compatibility with hosting environments.

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -3,6 +3,7 @@ import vituum from 'vituum';
 import posthtml from '@vituum/vite-plugin-posthtml';
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? '/conductor-framework/' : '/',
   plugins: [vituum(), posthtml({
     root: './src'
   })],
